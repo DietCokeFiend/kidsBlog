31abf274b75594f795b48d6cfa609952ce448705
DietCokeFiend <quinyung@gmail.com>
2013-05-23 09:48:37 -0400

    created header and footer includes, convert to php

---
+++ includes/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 		<html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width" />
+<title>Kids Summer Zone</title>
+
+<link rel="stylesheet" href="css/normalize.css" />
+<link rel="stylesheet" href="css/foundation.css" />
+<link rel="stylesheet" href="css/main.css" />
+
+
+<script src="js/vendor/custom.modernizr.js"></script>
+
+</head>
+<body>
+
+
+<!-- Nav Bar -->
+
+<div class="row">
+<header class="large-12 columns">
+	<div class="nav-bar right">
+	   <ul class="button-group">
+		<li><a href="#" class="button">Link 1</a></li>
+		 <li><a href="#" class="button">Link 2</a></li>
+		 <li><a href="#" class="button">Link 3</a></li>
+		 <li><a href="#" class="button">Link 4</a></li>
+		</ul>
+	</div>
+	<h1>
+		<a href="index.php"><img src="img/logo.png" class="logo" alt="Kids Summer Zone Logo" title="Summer Kids Zone Home"/></a>
+	</h1>
+	<div class="row">
+		<div class="large-12 columns">
+			<h6>The #1 online source for getting rid of your kids for the summer in the Greater Toronto Area.</h6>
+		</div>
+	</div>
+	<hr />
+</header>
+</div>
+
+<!-- End Nav -->
