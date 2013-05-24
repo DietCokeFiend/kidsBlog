18bba12db9b0aac00d335a6f043baffe963b5ba8
DietCokeFiend <quinyung@gmail.com>
2013-05-24 15:56:35 -0400

    added meta tags, smeh

--- summer-gaming-for-kids.php
+++ summer-gaming-for-kids.php
@@ -5,6 +5,10 @@
 <head>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width" />
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<meta name="Description" content="Summer Gaming for Kids">
+<meta name="keywords" content="Summer mobile gaming for kids like where's my water fruit ninja temple run angry birds " >
+<meta name="author" content="Grandmaster Q">
 <title>Summer Gaming For Kids - Kids Summer Zone</title>
 
 
@@ -22,7 +26,7 @@
 	<h2><a href="#">Gaming Distractions for the Summer</a></h2>
 	<h6>
 		<img src="img/quin.jpg" class="profile-img" alt="Image of Grandmaster Q" />
-		Written by <a href="#">Grandmaster Q. 
+		Written by <a href="#">Grandmaster Q.
 		</a> on May 21, 2013.
 	</h6>
 
@@ -52,68 +56,68 @@
 	</nav>
 	<hr>
   </article>
-  
+
   <div class="large-9 columns">
-  
+
 	<article>
 		<a id="angrybirds"></a>
 		<h3><a href="http://www.rovio.com/en/our-work/games/view/1/angry-birds" title="Go to Angry Birds Home Page">Angry Birds</a></h3>
 		<h6><a href="http://www.rovio.com/" title="Go to Rovio Entertainment Home Page">by Rovio Entertainment</a></h6>
-		
+
 		<p>Angry Birds have become a force to reckon with ever since its release.  Since then, there have been several iterations which includes a Star Wars version.  The objective of the game is to slingshot angry birds to topple structures and/or to crush various enemies.  Warning: this game is highly addictive and is recommended for everone.</p>
-		
+
 		<figure>
 			<img src="img/angry-birds1.jpg" alt="Angry Birds Screenshot - Title Screen" />
 			<img src="img/angry-birds2.jpg" alt="Angry Birds Screenshot - Attacking Structrure" />
 			<img src="img/angry-birds3.jpg" alt="Angry Birds Screenshot - Slingshot" />
 			<figcaption class="text-center"><small>[ Angry Birds images taken by Grandmaster Q.  Free to use. ]</small></figcaption>
 		</figure>
-		
+
 		<ul class="no-bullet">
 			<li>Developer: Rovio Entertainment</li>
 			<li>Price: Free for selected platform (PC/IOS/Android)</li>
 			<li>Rated: Everyone</li>
 		</ul>
-		
+
 		<hr>
 	</article>
-	
+
 	<article>
 		<a id="whereswater"></a>
 		<h3><a href="http://games.disney.com/wheres-my-water-app" title="Go to Where's My Water Home Page">Where's My Water</a></h3>
 		<h6><a href="http://games.disney.com" title="Go to Disney Games Home Page">by Disney Games</a></h6>
-		
+
 		<p>This is a game developed and published by Disney.  The premise is quite simple.  Mr. Aligator needs his water to take a shower or bath.  It is your goal to clear the path so that water can get to Mr. Aligator's water pipes.  I've seen kids play with this and it's hours upon hours of clicking and tapping fun on any of the prominent mobile devices.</p>
-		
+
 		<figure>
 			<img class="slender-img" src="img/wheres-my-water1.jpg" alt="Where's My Water Screenshot - Title Screen" />
 			<img class="slender-img" src="img/wheres-my-water2.jpg" alt="Where's My Water Screenshot - Bath Water Obtained" />
 			<img class="slender-img" src="img/wheres-my-water3.jpg" alt="Where's My Water Screenshot - Clear the Path" />
 			<figcaption class="text-center"><small>[ Where's My Water images taken by Grandmaster Q.  Free to use. ]</small></figcaption>
 		</figure>
-		
+
 		<ul class="no-bullet">
 			<li>Developer: Disney Games</li>
 			<li>Price: Free (PC/IOS/Android)</li>
 			<li>Rated: Everyone</li>
 		</ul>
-	
+
 		<hr>
 	</article>
-	
+
 	<article>
 		<a id="fruitninja"></a>
 		<h3><a href="http://fruitninja.com" title="Go to Fruit Ninja Home Page">Fruit Ninja</a></h3>
 		<h6><a href="http://halfbrick.com" title="Go to HalfBrick Studios Home Page">by HalfBrick Studios</a></h6>
-		
+
 		<p>Fruit Ninja is a very popular addictive game created by HalfBrick.  The premise is also very simple.  Fruit is thrown in the air.  It is up to you to slice them all before they drop out of the screen, all the while avoiding the bombs and creating epic combos.  The name of the game is getting the highest score.  Adorable graphics and easy to pick up gameplay makes this one addicting game for adults and kids alike. There are free versions and paid versions for both IOS and Android.</p>
-		
+
 
 		<figure>
 			<img src="img/fruit-ninja1.jpg"  alt="Fruit Ninja Screenshot - Title Screen"/>
 			<img src="img/fruit-ninja2.jpg" alt="Fruit Ninja Screenshot - Critical Combo" />
 			<img src="img/fruit-ninja3.jpg" alt="Fruit Ninja Screenshot - Fruit All Gone" />
-			
+
 			<figcaption class="text-center"><small>[ Fruit Ninja images taken by Grandmaster Q.  Free to use. ]</small></figcaption>
 		</figure>
 
@@ -125,22 +129,22 @@
 
 		<hr>
 	</article>
-	
+
 	<article>
 		<a id="templerun"></a>
 		<h3><a href="http://www.imangistudios.com/" title="Go to Imangi Studios Home Page">Temple Run</a></h3>
 		<h6><a href="http://www.imangistudios.com/" title="Go to Imangi Studios Home Page">by Imangi Studios</a></h6>
-		
+
 		<p>Simple controls and infinite gameplay makes this one game hard to resist for your little ones.  Run through a course, avoid obstacles and collect the gold!  Did I mention the controls are simple? Only four possible moves: flick up to jump, down to slide, left to move left and right to move right.  Free for both IOS and Android</p>
-		
+
 		<figure>
 			<img class="slender-img" src="img/temple-run1.jpg"  alt="Temple Run Screenshot - Title Screen" />
 			<img class="slender-img" src="img/temple-run2.jpg" alt="Temple Run Screenshot - Turning Corners" />
 			<img class="slender-img" src="img/temple-run3.jpg" alt="Temple Run Screenshot - Gold is Close By" />
-			
+
 			<figcaption class="text-center"><small>[ Temple Run images taken by Grandmaster Q.  Free to use. ]</small></figcaption>
 		</figure>
-		
+
 		<ul class="no-bullet">
 			<li>Developer: Imangi Studios</li>
 			<li>Price: Free (IOS/Android)</li>
@@ -154,7 +158,7 @@
 	  <div class="large-9 columns">
 		<p>There you have it, <i>folks</i>. Great games to <del>distract</del> entertainment your little ones for a bit.</p>
 	  </div>
-  </div>			
+  </div>
 </div>
 <!-- End Main Content -->
 
