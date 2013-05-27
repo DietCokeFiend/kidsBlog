15c0676b01263306c4ebf6e9b1c101d783384844
kalharip <kalhari_p@yahoo.ca>
2013-05-27 13:58:55 -0400

    Changed keywords,so

--- summer-attractions.php
+++ summer-attractions.php
@@ -7,10 +7,10 @@
 	<meta name="viewport" content="width=device-width" />
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 	<meta name="Description" content="Summer attractions">
-	<meta name="keywords" content="attractions,summer, CT tower, Marineland, African Lion safari, Science center " >
+	<meta name="keywords" content="attractions, summer, CT tower, Marineland, African Lion safari, Science center, kids " >
 	<meta name="author" content="Kalhari Perera">
 
-  <title>Places to visit in summer</title>
+  <title>Places to visit in summertime</title>
 
 
 <?php include_once("includes/header.php"); ?>
@@ -20,7 +20,7 @@
 	<div class="row">
 
 		<div class="large-10 columns">
-			<h2> Places to visit with your kids in summer</h2>
+			<h2> Places to visit with your kids in Summertime</h2>
 			 <h6>Written by Kalhari Perera - on May 22, 2013.</h6>
 
 			<div class="panel">
@@ -34,7 +34,7 @@
 
 					<div class="panel">
                     <h3>CN Tower</h3>
-						<p>The CN Tower is Canada's most wonderful architectural triumph with Toronto's most spectacular views in day and night.You can explore two observation levels up to 1,136 ft/346m, the world famous Glass Floor at 1,122 ft./342m with outdoor SkyTerrace, and access to attractions in the CN Tower story with Excellence film, motion theatre ride and more. It is a 553.33 m-high (1,815.4 ft)concrete communications and observation tower in Downtown Toronto, Ontario, Canada. It has built in 1976,becoming the world's tallest free-standing structure and world's tallest tower at that time. CN tower was declared as one of the  modern Seven Wonders of the World by the American Society of Civil Engineers. It attracts more than two million international visitors annually.This is one of the best place for you to visit with your kids.<a href="http://www.cntower.ca/" title="CN Tower website">Go to CN Tower website</a></p>
+						<p>The CN Tower is Canada's most wonderful architectural triumph with Toronto's most spectacular views in day and night.You can explore two observation levels up to 1,136 ft/346m, the world famous Glass Floor at 1,122 ft./342m with outdoor SkyTerrace, and access to attractions in the CN Tower story with Excellence film, motion theatre ride and more. It is a 553.33 m-high (1,815.4 ft)concrete communications and observation tower in Downtown Toronto, Ontario, Canada. It has built in 1976,becoming the world's tallest free-standing structure and world's tallest tower at that time. CN tower was declared as one of the  modern Seven Wonders of the World by the American Society of Civil Engineers. It attracts more than two million international visitors annually.This is one of the best place for you to visit with your children.<a href="http://www.cntower.ca/" title="CN Tower website">Go to CN Tower website</a></p>
 
                         <img src="img/cntower.jpg" alt="CN Tower" >
 					</div>
@@ -45,7 +45,7 @@
 					<div class="panel">
 					<h1>African Lion Safari</h1>
 
-		<p>African Lion Safari is a family-owned safari park,just an hour outside of Toronto. It has all the wonders of Africa including doors locked and windows up, a one-hour drive through the reserve takes families past grazing giraffes, exotic birds and majestic lions. You can also leave your car parked and take a guided bus tour through the safari for an additional fee. You can see live animal performances including the Parrot Paradise Show, Birds of Prey Flying Demonstration and Elephant Round-Up, or watch the Elephant Swim in the recreational lake. African Lion Safari also provides picnic areas, the boat tour and Misumu Bay water play area for the kids.<a href="http://www.lionsafari.com/" title="African Lion Safari website">Go to African Lion Safari website</a></p>
+		<p>African Lion Safari is a family-owned safari park,just an hour outside of Toronto. It has all the wonders of Africa including doors locked and windows up, a one-hour drive through the reserve takes families past grazing giraffes, exotic birds and majestic lions. You can also leave your car parked and take a guided bus tour through the safari for an additional fee. You can see live animal performances including the Parrot Paradise Show, Birds of Prey Flying Demonstration and Elephant Round-Up, or watch the Elephant Swim in the recreational lake. African Lion Safari also provides picnic areas, the boat tour and Misumu Bay water play area for the little ones.<a href="http://www.lionsafari.com/" title="African Lion Safari website">Go to African Lion Safari website</a></p>
 
 
 					<img src="img/lionsafari.jpg" alt="African Lion Safari" >
