1deeec035c284fb99dbb8c60a1f06b376dd2f0bb
Mercy Loachamin <lloachamin@hotmail.com>
2013-05-27 12:20:54 -0400

    changing kids and summer words

--- summer-outdoor-activities.php
+++ summer-outdoor-activities.php
@@ -23,13 +23,13 @@
 
       <article>
 
-        <h3><a href="#">Summer Outdoor Activities for your Kids</a></h3>
+        <h3>Summer Outdoor Activities for your Kids</h3>
         <h6>Written by Mercy Loachamin - on May 22, 2013.</h6>
 
 		<div class="row">
 		<div class="large-12 columns">
 		<img src="img/smiling-kids.jpeg" alt="smiling kids on the grass" title="smiling kids"> 
-		<p>Summer is almost here!! Let kids have a lot of fun outdoors, their age demands to explore the nature as everything they find outdoors is new and exciting for them. They are curious about all what they see outdoors. Summer is a great time to enjoy these days in family, parents have to take the children outdoors frequently, playing at the garden with them or taking them to a nearby park would be a good start. Remember to enjoy your kids every moment and do not forget your camera to help you to capture those unforgettable moments.</p>
+		<p>Warmest season is almost here!! Let kiddies have a lot of fun outdoors, their age demands to explore the nature as everything they find outdoors is new and exciting for them. They are curious about all what they see outdoors. Summery weather is a great time to enjoy these days in family, parents have to take the children outdoors frequently, playing at the garden with them or taking them to a nearby park would be a good start. Remember to enjoy your little ones every moment and do not forget your camera to help you to capture those unforgettable moments.</p>
 		</div>
 		</div>
 		
@@ -37,7 +37,7 @@
         <div class="row">
           <div class="large-6 columns">
 		  <img src="img/baby-inside-flower.jpeg" alt="little baby inside a flower" title="baby inside flower">
-			  <p>Let your kids being part of the nature!! Kids love to get down and dirty in the soil. Why not to let them helping you to plant and taking care of plants, herbs and vegetables. It is not necessary to have a big garden for that; many plants grow well in pots or in a smallest backyard space.</p>	
+			  <p>Let your kiddies being part of the nature!! Young children love to get down and dirty in the soil. Why not to let them helping you to plant and taking care of plants, herbs and vegetables. It is not necessary to have a big garden for that; many plants grow well in pots or in a smallest backyard space.</p>	
             
           </div>
           <div class="large-6 columns">
@@ -52,14 +52,14 @@
 				<div class="large-6 columns">
 					
 						<img src="img/boy-inside-car.jpeg" alt="boy playing inside his toy car" title="boy playing with car">
-			  <p>Why not to take your kids to a park with their favorite toy, they will learn how to socialize, how to make new friends.
-			They will enjoy the company of other kids, both their age and older. Parents can teach the kids about sharing their toys. </p>
+			  <p>Why not to take your young child to a park with their favorite toy, they will learn how to socialize, how to make new friends.
+			They will enjoy the company of other little fellows, both their age and older. Parents can teach the little ones about sharing their toys. </p>
 					
 				</div>
 				<div class="large-6 columns">
 					
 						<img src="img/bathing-dog.jpeg" alt="family bathing dog" title="bathing a dog">
-			  <p>Having a pet at home helps your kid to develop love and compassion for animals. Your kid will be happy helping you bathing the dog, once in a month will be enough. But don't overdo it! Too many baths take away the natural oils from a pet's skin.</p>
+			  <p>Having a pet at home helps your kiddy to develop love and compassion for animals. Your little one will be happy helping you bathing the dog, once in a month will be enough. But don't overdo it! Too many baths take away the natural oils from a pet's skin.</p>
 					
 				</div>
 			</div>	
@@ -68,14 +68,14 @@
 				<div class="large-6 columns">
 					
 						<img src="img/boy-playing-ball.jpeg" alt="boy playing with a big ball" title="boy and a ball">
-			  <p>Discover the individual talents and skills of your kids when they are practicing sports. Sports gets the children moving, helping them build muscle, avoid obesity and also help to develop physical skills like coordination and agility. You need to consider that some children are good practicing one sport and others prefer another sport. It may take a few tries to find the right sport for your kid. </p>
+			  <p>Discover the individual talents and skills of your young children when they are practicing sports. Sports gets the children moving, helping them build muscle, avoid obesity and also help to develop physical skills like coordination and agility. You need to consider that some children are good practicing one sport and others prefer another sport. It may take a few tries to find the right sport for your kiddy. </p>
 						
 					
 				</div>
 				<div class="large-6 columns">
 					
 						<img src="img/boys-bike.jpeg" alt="two boys riding bike around trees" title="boy riding bike">
-			  <p>By encouraging kids to cycle from an early age and getting them to take their ability riding a bike, will equip them with a skill for life. </p>
+			  <p>By encouraging young children to cycle from an early age and getting them to take their ability riding a bike, will equip them with a skill for life. </p>
 					
 				</div>
 			</div>
